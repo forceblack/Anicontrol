bdd9472153657a717397b32defdcbb01f3055ff5
forceblack <clementfaurebrac@gmail.com>
2019-04-18 15:15:33 +0200

    Update Anicontrol.ino
    ajout du code issue de USB_Host_Shield_2.0-master/example/USBH_MIDI/USB_MIDI_converter.
    
    on dois choisir entre afficher ou envoyer mais desfois ca marche (souvent il faut redemarer l'appli et le dock pour que ce soit reconnu...)

--- Anicontrol.ino
+++ Anicontrol.ino
@@ -13,12 +13,25 @@
 #include <usbh_midi.h>
 #include <usbhub.h>
 
-// Satisfy the IDE, which needs to see the include statment in the ino too.
+// Satisfy the IDE, which needs to see the include statment in the ino too. ! Yeah !
 #ifdef dobogusinclude
 #include <spi4teensy3.h>
 #endif
 #include <SPI.h>
 
+
+//viens du code USB_MIDI_converter
+#ifdef USBCON
+#define _MIDI_SERIAL_PORT Serial1
+#else
+#define _MIDI_SERIAL_PORT Serial
+#endif
+//////////////////////////
+// MIDI Pin assign
+// 2 : GND
+// 4 : +5V(Vcc) with 220ohm
+// 5 : TX
+//////////////////////////
 
 USB Usb;
 
@@ -26,11 +39,14 @@ USBH_MIDI  Midi(&Usb);
 
 void MIDI_poll();          //Affichage de signal Midi de L'USB vers le Serial Monitor
 void MIDI_sendToCable();    //On va essayer d'envoyer aussi l'info dans le Pin Tx de l'arduino
-
+void doDelay(uint32_t t1, uint32_t t2, uint32_t delayTime); //ajout delay
 
 void setup()
 {
-  Serial.begin(115200);
+
+  _MIDI_SERIAL_PORT.begin(31250);
+  //attention, je dois choisir si je veux afficher ou si je veux envoyer dans le cable donc soit la ligne du dessus, soit celle du dessous
+//  Serial.begin(115200);
   if (Usb.Init() == -1) 
     {
       while (1); //halt
@@ -41,13 +57,17 @@ void setup()
 void loop()
 {
   Usb.Task();
+   uint32_t t1 = (uint32_t)micros(); //ajout delay
   if ( Midi ) {
-    MIDI_poll(); //On affiche (ca sert toujours)
+ //   MIDI_poll();        //On affiche (ca sert toujours)
     MIDI_sendToCable(); //On envoie (ca c'est pas encore fini)
   }
+
+    //delay(1ms) a priori, sans ca marche aussi...
+  doDelay(t1, (uint32_t)micros(), 1000);
 }
 
-// Poll USB MIDI Controler and send to serial MIDI
+// Poll USB MIDI Controler and send to serial MIDI ! Yeah !
 void MIDI_poll()
 {
   char buf[20];
@@ -59,7 +79,7 @@ void MIDI_poll()
 
     //formatage d'affichage dans le Serial Monitor
     uint32_t time = (uint32_t)millis();
-    sprintf(buf, "%04X%04X: ", (uint16_t)(time >> 16), (uint16_t)(time & 0xFFFF)); // Split variable to prevent warnings on the ESP8266 platform
+    sprintf(buf, "%04X%04X: ", (uint16_t)(time >> 16), (uint16_t)(time & 0xFFFF)); // Split variable to prevent warnings on the ESP8266 platform ! Yeah ! 
     Serial.print(buf);
     Serial.print(rcvd);
     Serial.print(':');
@@ -76,5 +96,27 @@ void MIDI_poll()
 
 void MIDI_sendToCable()
 {
+//  Serial.println("Je suis la");
+  uint8_t outBuf[ 3 ];
+  uint8_t size;
+
+  do {
+    if ( (size = Midi.RecvData(outBuf)) > 0 ) {
+      //MIDI Output
+      _MIDI_SERIAL_PORT.write(outBuf, size);
+ //     Serial.println("Je suis dans le do");
+    }
+  } while (size > 0);
   
+}
+
+// Delay time (max 16383 us)
+void doDelay(uint32_t t1, uint32_t t2, uint32_t delayTime)
+{
+  uint32_t t3;
+
+  t3 = t2 - t1;
+  if ( t3 < delayTime ) {
+    delayMicroseconds(delayTime - t3);
+  }
 }
